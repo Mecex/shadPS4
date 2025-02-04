fabcf129ed511d41bd09a97542e2cee5b48b3022
Mecex <mecex.gamer17@gmail.com>
2025-02-04 19:07:46 +0100

    Update vk_pipeline_cache.cpp

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -259,7 +259,7 @@ bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
     static std::vector<u64> skip_hashes = {
 
         //Required to get Lies of P to menus
-        0x7eaca10c670a8c03,
+        
 
         //Sekiro gs shader before "Ring Access Elimination 131" assert, and cs shader which drops FPS
         0x7ee03d3f,
